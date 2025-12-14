4c2f90df1bba701ec6e5e14a1d229e3c15aef8f7
Rohan Korake <rohannkorake@gmail.com>
2025-12-14 14:09:08 +0530

    add concepts folder

--- Concepts/scope.cpp
+++ Concepts/scope.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int globalVar=10;
+int main()
+{
+    int globalVar = 10;   // Variable declared in main() scope
 
-    if(true)
+    if (true)
     {
-        int localVar=20;
-        cout<<"value of local vaiable inside the scope "<<localVar<<endl;
-        cout<<"value of global variable inside the local scope "<<globalVar<<endl;
+        int localVar = 20;   // Variable declared inside if-block (local scope)
 
+        cout << "Value of local variable inside the block: "
+             << localVar << endl;
+
+        cout << "Value of global variable inside the block: "
+             << globalVar << endl;
     }
-    cout<<"value of global variable is "<<globalVar<<endl;
-    // cout<<"value of local vaiable outside the scope "<<localVar<<endl; //this line shows error because localVar is the local variable not accessible outside the scope
 
-   return 0;
+    // globalVar is accessible here because it is in the outer scope
+    cout << "Value of global variable outside the block: "
+         << globalVar << endl;
+
+    // ERROR: localVar is not accessible outside the if-block
+    // cout << "Value of local variable outside the block: " << localVar << endl;
+
+    return 0;
 }
